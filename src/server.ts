6d93995c328c3c3e75c4eff7f12e59392a5d4d53
unknown <safayetnurelectra@gmail.com>
2023-06-07 07:08:49 +0600

    extension setup updated

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function bootstrap() {
-    try{
-        await mongoose.connect(config.database_url as string);
-        console.log('database added');
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('database added')
 
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
-          })
-    }
-    catch(err){
-        console.log('failed to connect',err)
-    }
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('failed to connect', err)
   }
-bootstrap();
+}
+bootstrap()
